14edfd6d7091607ba946178f030eb68caad72754
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-10-01 11:32:48 +0200

    update generate_esgfconf.sh and add new jar lists

--- generate_esgfconf.sh
+++ generate_esgfconf.sh
@@ -5,7 +5,7 @@ if ! [ -s cog_packages  ] || ! [ -s pub_packages ] || ! [ -s jarlist ]; then
     exit -1;
 fi
 echo -n >esgf.conf
-for i in cog_packages pub_packages jarlist; do
+for i in cog_packages pub_packages jarlist lasjars solr_webappjars solr_serverjars; do
     echo -n >$i.out
     while read ln; do
         pkg=`basename $ln`;
@@ -17,8 +17,7 @@ for i in cog_packages pub_packages jarlist; do
         echo $pkg|cut -d '=' -f1|perl -pe 's|(.*?)-[0-9].*|\1|' >>$i.out
     done <$i
 done
-excludes=`cat esgf_excludes`
-packages=`cat esgf_manual cog_packages.out jarlist.out pub_packages.out|sort -u|paste -sd,`
+packages=`cat esgf_manual cog_packages.out jarlist.out pub_packages.out lasjars.out solr_webappjars.out solr_serverjars.out|sort -u|paste -sd,`
 echo $packages >packages
 while read ln; do
     sed -i "s/,$ln,/,/" packages
@@ -26,4 +25,5 @@ done <expungelist
 packages=`cat packages`
 rm packages
 echo "packages=$packages" >>esgf.conf
-echo "excludes=$excludes" >>esgf.conf
+cat esgf_keywords >>esgf.conf
+cat esgf_excludes >>esgf.conf
