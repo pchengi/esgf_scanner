d7fbcba60e28678e24027014eb1f3480d1a3af31
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-10-10 15:40:52 +0200

    update script

--- rungetpackagelists.sh
+++ rungetpackagelists.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo -n >packagelists.tgz
 if [ $# -lt 1 ]; then 
     echo "need hostname of ESGF node on which you can perform a passwordless-ssh";
     exit -1
@@ -9,3 +9,9 @@ esgfhost=$1
 scp getpackagelists.sh root@${esgfhost}:
 ssh -l root ${esgfhost} bash /root/getpackagelists.sh
 scp root@${esgfhost}:/root/packagelists.tgz .
+
+if [ ! -s packagelists.tgz ]; then
+    echo "Problem retrieving packagelists";
+    exit -1;
+fi
+tar -xzf packagelists.tgz
