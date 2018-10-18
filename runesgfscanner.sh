5dd431160ccbd689c180e7bec3228a951e7cf538
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-10-18 13:45:44 +0200

    update runesgfscanner

--- runesgfscanner.sh
+++ runesgfscanner.sh
@@ -19,34 +19,38 @@ sender=`grep alertmail_sender esgf_scanner.conf|cut -d '=' -f2`
 port=`grep mailserver_port esgf_scanner.conf|cut -d '=' -f2`
 server=`grep mailserver_host esgf_scanner.conf|cut -d '=' -f2`
 
-git pull >/dev/null && git submodule init >/dev/null && git submodule update >/dev/null
+#git pull >/dev/null && git submodule init >/dev/null && git submodule update >/dev/null
 bash firstuse.sh
 bash generate_esgfconf.sh
 cp esgf.conf exportedmutes cvechecker
+cp ack cvechecker/pinned_cves
 cd cvechecker
-python3 cvechecker.py -u >>runlog-esgf_scanner
+rm unmutable
+#python3 cvechecker.py -u >>runlog-esgf_scanner
 python3 cvechecker.py -i exportedmutes >muting_transcript
 if [ -s muting_transcript ]; then
 	cat muting_transcript|awk '{print $4}' >unmutable
+    cat unmutable|paste -sd, >unmutable_cvelist
 fi
 python3 cvechecker.py -r esgf.conf >esgfreport.txt
 cd ..
+bash cvechecker/getcvelist.sh cvechecker/esgfreport.txt one-per-line >currcves
 bash check_for_new.sh cvechecker/esgfreport.txt >newcves
 if [ -s cvechecker/unmutable ]; then
     firstoutdated='True';
     firstupdated='True';
     outdatedcves=''
 	while read unm; do
-		if ! grep -w $unm newcves >/dev/null; then
+		if ! grep -w $unm currcves >/dev/null; then
             if [ "$firstoutdated" = "True" ]; then
-                firstoutdated = "False";
+                firstoutdated="False";
                 outdatedcves=$unm;
             else
                 outdatedcves=${outdatedcves},$unm;
             fi
         else
             if [ "$firstupdated" = "True" ]; then
-                firstupdated = "False";
+                firstupdated="False";
                 updatedcves=$unm;
             else
                 updatedcves=${updatedcves},$unm;
@@ -54,20 +58,28 @@ if [ -s cvechecker/unmutable ]; then
         fi
     done <cvechecker/unmutable
     if [ "$outdatedcves" != "" ]; then
+        cd cvechecker 
+        python3 cvechecker.py -c $outdatedcves >../outdatedcvesreport.txt
+        cd ..
         echo "We are asking to mute the following CVEs which are not showing up against any configured products. Perhaps these are candidates for removal from exported mutes?";
         echo "$outdatedcves";
+        echo "$outdatedcves" >outdatedcves_list.txt;
     fi
     if [ "$updatedcves" != "" ]; then
-        echo "We are asking to mute the following CVEs which are not showing up against any configured products. Perhaps these are candidates for removal from exported mutes?";
-       echo "$updatedcves";
+        cd cvechecker
+        python3 cvechecker.py -c $updatedcves >../updatedcvesreport.txt
+        cd ..
+        echo "We are asking to mute the following CVEs which seem to have been updated";
+        echo "$updatedcves";
+        echo "$updatedcves" >updatedcves_list.txt;
     fi
 fi
-
-               
-			
-
-
-
-
-
-
+if [ -s newcves ]; then
+    echo "We have new CVE hits against our packages."
+    newcvelist=`cat newcves|paste -sd,`
+    echo $newcvelist
+    cd cvechecker
+    python3 cvechecker.py -c $newcvelist >../newcvereport.txt
+    cd ..
+fi
+rm esgfscannerrun
